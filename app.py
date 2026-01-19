0c9e197c18bb99f1f842cd16ec1240393da56af0
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2026-01-19 17:42:37 +0000

    Adicionando o bd, colocando as classes iniciais, configuração inicial para login

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request
 
 livros = []
+import json
+from flask import flash, redirect
+from utils import db, lm
+import os
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_usuario = os.getenv('DB_USERNAME')
+db_senha = os.getenv('DB_PASSWORD')
+db_mydb = os.getenv('DB_DATABASE')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_mydb}"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+lm.init_app(app)
+migrate = Migrate(app, db)
 
 app = Flask(__name__)
 
