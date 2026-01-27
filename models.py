8b95521ab4f10f2a8d92e9770a565736cb9c425e
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2026-01-27 17:25:43 +0000

    adicionando os arquivos que prof. alba pediu

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Usuario(db.Model, UserMixin):
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
     senha = db.Column(db.String(100))
-    ...
+    
 
     def __init__(self, nome, email, senha):
         self.nome = nome
