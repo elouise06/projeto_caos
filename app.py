6d24d8eeeb89b79c7c3fd617a2707fd76e4c5b40
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2026-02-05 11:47:55 +0000

    finalizar isso logo

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request
-
-
-livros = []
-
-import json
-from flask import flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session
 from utils import db, lm
 import os
 from flask_migrate import Migrate
+from models import Usuario, Livro
+from flask_login import login_required, current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -17,6 +15,7 @@ db_mydb = os.getenv('DB_DATABASE')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 
+
 conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_mydb}"
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,17 +24,109 @@ db.init_app(app)
 lm.init_app(app)
 migrate = Migrate(app, db)
 
-app = Flask(__name__)
-
 @app.route('/')
+def index():
+    return render_template('caos.html')
+
+@app.route('/cadastro/etapa1', methods=['GET', 'POST'])
+def cadastro_etapa1():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        telefone = request.form.get('telefone')
+
+        if not nome or not email or not telefone:
+            flash("Preencha todos os campos", "warning")
+            return redirect('/cadastro/etapa1')
+
+        
+        session['cadastro'] = {
+            'nome': nome,
+            'email': email,
+            'telefone': telefone
+        }
+
+        return redirect('/cadastro/etapa2')
+
+    return render_template('cadastro-dados.html')
+
+@app.route('/cadastro/etapa2', methods=['GET', 'POST'])
+def cadastro_etapa2():
+    if 'cadastro' not in session:
+        flash("Complete a primeira etapa do cadastro", "warning")
+        return redirect('/cadastro/etapa1')
+    
+    if request.method == 'POST':
+        nome_usuario = request.form.get('nome_usuario')
+        senha = request.form.get('senha')
+
+        if not nome_usuario or not senha:
+            flash("Preencha todos os campos", "warning")
+            return redirect('/cadastro/etapa2')
+
+        
+        dados = session['cadastro']
+        nome = dados['nome']
+        email = dados['email']
+        telefone = dados['telefone']
+
+        senha_hash = generate_password_hash(senha)
+        novo_usuario = Usuario(
+            nome=nome,
+            email=email,
+            telefone=telefone,
+            nome_usuario=nome_usuario,
+            senha=senha_hash
+        )
+
+        db.session.add(novo_usuario)
+        db.session.commit()
+
+        
+        session.pop('cadastro')
+
+       
+        return redirect('/cadastro/sucesso')
+
+    return render_template('cadastro-dados-2.html')
+
+@app.route('/cadastro/sucesso')
+def cadastro_sucesso():
+    return render_template('cadastro-fim.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        usuario = Usuario.query.filter_by(email=email).first()
+
+        if usuario and check_password_hash(usuario.senha, senha):
+            login_user(usuario)
+            return redirect('/inicio')
+        else:
+            flash('Dados incorretos')
+            return redirect('/login')
+
+    return render_template('login.html')
+
+@lm.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
+
+@app.route('/inicio')
+
 def inicio():
     return render_template('inicio.html')
 
 @app.route('/comunidade')
+
 def comunidade():
     return render_template('comunidade.html')
 
 @app.route('/sobre')
+
 def sobre():
     return render_template('sobre.html')
 
@@ -47,40 +138,9 @@ def mercado():
 def doacao():
     return render_template('doacao.html')
 
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro-dados.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/cadastrarlivro')
-def cadastrarlivro():
-    return render_template('cadastrar_livro.html')
-
-@app.route('/verlivro', methods=['POST'])
-def verlivro():
-    nome = request.form['nome']
-    autor = request.form['autor']
-    valor = float(request.form['valor'])
-    if valor <= 30:
-        mensagem = 'Esse livro está apto para as vendas!'
-    else:
-        mensagem = 'Esse livro não está apto para as vendas!'
-
-    livro = {
-        'nome': nome,
-        'autor': autor,
-        'valor': valor,
-        'mensagem': mensagem
-    }
-    livros.append(livro)
-    return render_template('livro_cadastrado.html',livro = livro)
-
-@app.route('/listalivros')
-def listalivros():
-    return render_template('lista_livro.html', livros=livros)
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil_usuario.html')
 
 if __name__ == "__main__":
     app.run()
--- models.py
+++ models.py
@@ -1,18 +1,26 @@
 from utils import db
 from flask_login import UserMixin
+from datetime import datetime, timezone
 
 class Usuario(db.Model, UserMixin):
     __tablename__= "usuario"
     id = db.Column(db.Integer, primary_key = True)
-    nome = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    senha = db.Column(db.String(100))
-    
+    nome = db.Column(db.String(100), nullable=False)
+    nome_usuario = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    senha = db.Column(db.String(300), nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
 
-    def __init__(self, nome, email, senha):
+    livros = db.relationship('Livro', backref='usuario', lazy=True)
+    posts = db.relationship('Post', backref='usuario', lazy=True)
+    comentarios = db.relationship('Comentario', backref='usuario', lazy=True)
+
+    def __init__(self, nome, email, senha, nome_usuario, telefone):
         self.nome = nome
+        self.nome_usuario = nome_usuario
         self.email = email
         self.senha = senha
+        self.telefone = telefone
         
     
     def __repr__(self):
@@ -21,16 +29,67 @@ class Usuario(db.Model, UserMixin):
 class Livro(db.Model):
     __tablename__= "livro"
     id = db.Column(db.Integer, primary_key = True)
-    ...
-    titulo = db.Column(db.String(100))
-    autor = db.Column(db.String(100))
-    editora = db.Column(db.String(100))
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+    genero = db.Column(db.String(100), nullable=False)
+    ano_publicacao = db.Column(db.String(100), nullable=False)
+    valor_desejado = db.Column(db.String(100))
 
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
     
-
-    def __init__(self, titulo):
+    def __init__(self, titulo, autor, genero, ano_publicacao, valor_desejado):
         self.titulo = titulo
-        ...
+        self.autor = autor
+        self.genero = genero
+        self.ano_publicacao = ano_publicacao
+        self.valor_desejado = valor_desejado
     
     def __repr__(self):
-        return "<Livro {}>".format(self.titulo)
+        return "<Livro {} - autor {}>".format(self.titulo, self.autor)
+    
+class Post(db.Model):
+    __tablename__="post"
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.Text, nullable=False)
+    data_postagem = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc))
+    
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    comentarios = db.relationship('Comentario', backref='post', lazy=True)
+
+    def __repr__(self):
+        return "<Post {} do usuário {}>".format(self.id, self.usuario_id)
+    
+class Avaliacao(db.Model):
+    __tablename__="avaliacao"
+    id = db.Column(db.Integer, primary_key=True)
+    nota = db.Column(db.Integer, nullable=False)
+    comentario = db.Column(db.Text, nullable=False)
+
+    avaliador_id = db.Column(db.Integer,db.ForeignKey('usuario.id'),nullable=False)
+    avaliado_id = db.Column(db.Integer,db.ForeignKey('usuario.id'),nullable=False)
+
+    avaliador = db.relationship('Usuario',foreign_keys=[avaliador_id],backref='avaliacoes_feitas')
+    avaliado = db.relationship('Usuario',foreign_keys=[avaliado_id],backref='avaliacoes_recebidas')
+
+    def __init__(self, nota, comentario, avaliador_id, avaliado_id):
+        if nota < 1 or nota > 5:
+            raise ValueError("Nota inválida")
+        self.nota = nota
+        self.comentario = comentario
+        self.avaliador_id = avaliador_id
+        self.avaliado_id = avaliado_id
+
+    def __repr__(self):
+        return "<Avaliacao {}>".format(self.id)
+    
+class Comentario(db.Model):
+    __tablename__="comentario"
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.Text, nullable=False)
+    data_postagem = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc))
+    
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return "<Comentario {}>".format(self.id)
